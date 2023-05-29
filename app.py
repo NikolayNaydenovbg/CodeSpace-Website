2f30800ce0e4a3039eeb998ebc90dbe35592ec19
Nikolay <Nikolai889969@gmail.com>
2023-05-29 13:10:33 +0100

    Adds Flask and Create Account/ Sign-in

--- app.py
+++ app.py
@@ -1,47 +1,105 @@
-import mysql.connector
-from flask import Flask, make_response, request
-
-mydb = mysql.connector.connect(
-    host='localhost',
-    user='root',
-    password='930826',
-    port='3306',
-    database='michal_kors'
-)
+import sqlite3
+from flask import Flask, make_response, request, render_template
 
 app = Flask(__name__)
 
 
+@app.route('/')
+def homepage():
+    return render_template('homepage.html')
+
+
 @app.route('/about')
-def public():
-    cursor = mydb.cursor()
-
-    cursor.execute('SELECT * FROM users')
-
-    users = cursor.fetchall()
-
-    return make_response(users)
-
-@app.route('/users')
-def index():
-    cursor = mydb.cursor()
-
-    cursor.execute('SELECT * FROM users')
-
-    users = cursor.fetchall()
-
-    return make_response(users)
+def about():
+    return render_template('about.html')
 
 
-@app.route('/users', methods=["POST"])
-def index_post():
-    cursor = mydb.cursor()
+@app.route('/men')
+def men():
+    return render_template('mens.html')
 
-    cursor.execute('SELECT * FROM users')
 
-    users = cursor.fetchall()
-    print(request.get_json())
-    return make_response("HEllo!")
+@app.route('/women')
+def women():
+    return render_template('women.html')
+
+
+@app.route('/shoes')
+def shoes():
+    return render_template('shoes.html')
+
+
+@app.route('/clothing')
+def clothing():
+    return render_template('clothing.html')
+
+
+@app.route('/bags')
+def bags():
+    return render_template('bags.html')
+
+
+@app.route('/smart-watches')
+def smart_watches():
+    return render_template('smart-watches.html')
+
+
+@app.route('/mens-jackets')
+def mens_jackets():
+    return render_template('mens-jackets.html')
+
+
+@app.route('/sign-in', methods=["POST", "GET"])
+def sign_in():
+    conn = sqlite3.connect('database.db')
+
+    cursor = conn.cursor()
+
+    if request.method == 'GET':
+        return render_template('sign-in.html')
+    else:
+        email = request.form['email']
+        password = request.form['password']
+        if not email or not password:
+            return render_template('sign-in.html', message='Email or password is empty')
+
+        query = "SELECT COUNT(email) FROM users WHERE email = '{}'".format(email)
+        cursor.execute(query)
+        result = cursor.fetchone()
+
+        if result[0] == 0:
+            return render_template('sign-in.html', message='That account does not exist.')
+
+        return render_template('sign-in.html', message='You are signed in.')
+
+
+@app.route('/create-account', methods=["POST", "GET"])
+def create_account():
+    conn = sqlite3.connect('database.db')
+
+    cursor = conn.cursor()
+
+    if request.method == 'GET':
+        return render_template('create-account.html')
+    else:
+        username = request.form['username']
+        email = request.form['email']
+        password = request.form['password']
+        password_confirmation = request.form['password_confirmation']
+
+        if password != password_confirmation:
+            return render_template('create-account.html', message='Passwords don\'t match.')
+
+        if not email or not password or not username:
+            return render_template('create-account.html', message='Email, password or username are empty')
+
+        query = "INSERT INTO main.users (username, email, password) VALUES('{}', '{}', '{}')".format(username, email, password)
+
+        cursor.execute(query)
+        conn.commit()
+        conn.close()
+
+        return render_template('sign-in.html', message='Your account has been created. Please sign in.')
 
 
 if __name__ == "__main__":
